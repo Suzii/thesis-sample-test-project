c1daffac5753659a05d3754b3715c599ff69561d
Suzii <z.dankovcikova@gmail.com>
2017-01-29 19:36:43 +0100

    Add generator for ValidationHelperGet

---
+++ SampleProjectGenerator/SampleProjectGenerator/CodeGenerators/ConsoleApp/Implementation/ValidationHelperGet.cs
@@ -0,0 +1,48 @@
+﻿namespace SampleProjectGenerator.CodeGenerators.ConsoleApp.Implementation
+{
+    public class ValidationHelperGet : BaseConsoleAppClassCodeGenerator
+    {
+        public override string GetDocumentName()
+        {
+            return nameof(ValidationHelperGet);
+        }
+
+        protected override int NumberOfDiagnosticsInBody { get; } = 8;
+        
+        protected override string GetClassPrefix()
+        {
+            return $@"using System;
+using System.Globalization;
+using CMS.Helpers;
+
+namespace ConsoleApp.Controllers
+{{
+    public class {GetDocumentName()}
+    {{";
+        }
+
+        protected override string GetClassBodyToRepeat(int iterationNumber)
+        {
+            return $@"
+        public void SampleMethodA{iterationNumber}()
+        {{
+            var a1 = ValidationHelper.GetDouble("""", 0);
+            var a1 = ValidationHelper.GetDouble(""2"", 2);
+            var a2 = ValidationHelper.GetDouble("""", 0, CultureInfo.CurrentCulture);
+        }}
+
+        public void SampleMethodB{iterationNumber}()
+        {{
+            var a1 = ValidationHelper.GetDateTime("""", DateTime.MaxValue);
+            var a2 = ValidationHelper.GetDateTime("""", DateTime.MaxValue, ""en-us"");
+            var a3 = ValidationHelper.GetDateTime("""", DateTime.MaxValue, CultureInfo.CurrentCulture);
+        }}
+
+        public void SampleMethodC{iterationNumber}()
+        {{
+            var a1 = ValidationHelper.GetDate("""", DateTime.MaxValue);
+            var a3 = ValidationHelper.GetDate("""", DateTime.MaxValue, CultureInfo.CurrentCulture);
+        }}";
+        }
+    }
+}
--- SampleProjectGenerator/SampleProjectGenerator/CodeGenerators/WebApp/Implementation/ClientScriptMethods.cs
+++ SampleProjectGenerator/SampleProjectGenerator/CodeGenerators/WebApp/Implementation/ClientScriptMethods.cs
@@ -11,7 +11,7 @@
 
         protected override string GetClassPrefix()
         {
-            return $@"namespace WebApplication1.Controllers
+            return $@"namespace WebApp.Controllers
 {{
     public class {GetDocumentName()} : System.Web.UI.Control
     {{";

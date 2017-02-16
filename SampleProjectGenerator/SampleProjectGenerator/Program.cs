f756a535e085f99076d55a85f2e33c453928b27b
zuzanad <zuzanad@kentico.com>
2017-02-16 11:00:42 +0100

    Make Generator main method configurable

--- SampleProjectGenerator/SampleProjectGenerator/Program.cs
+++ SampleProjectGenerator/SampleProjectGenerator/Program.cs
@@ -8,6 +8,31 @@ namespace SampleProjectGenerator
 {
     internal class Program
     {
+        private static readonly int DesiredNumberOfDiagnosticsPerFile = 1000;
+
+        private static readonly int NumberOfFilesPerAnalyzer = 1;
+        
+        private static void Main(string[] args)
+        {
+            var sampleProjectFolder = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\..\SampleProject"));
+            var classCodeGenerators = GetAllClassCodeGenerators();
+
+            foreach (var codeGenerator in classCodeGenerators)
+            {
+                var generatedClasses = codeGenerator.GenerateClasses(DesiredNumberOfDiagnosticsPerFile, NumberOfFilesPerAnalyzer);
+
+                for (int i = 0; i < NumberOfFilesPerAnalyzer; i++)
+                {
+                    var documentPath = Path.Combine(sampleProjectFolder, RelativeDocumentPaths.Get(codeGenerator.ProjectType), codeGenerator.GetDocumentName(i) + ".cs");
+                    File.WriteAllText(documentPath, generatedClasses[i]);
+                }
+            }
+
+            Console.WriteLine($"Project files generated to {sampleProjectFolder}");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
         private static IEnumerable<IClassCodeGenerator> GetAllClassCodeGenerators()
         {
             var generatorInterface = typeof(IClassCodeGenerator);
@@ -18,22 +43,6 @@ namespace SampleProjectGenerator
                 .Cast<IClassCodeGenerator>();
 
             return codeGenerators;
-        }
-
-        private static void Main(string[] args)
-        {
-            var sampleProjectFolder = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\..\SampleProject"));
-            var classCodeGenerators = GetAllClassCodeGenerators();
-
-            foreach (var codeGenerator in classCodeGenerators)
-            {
-                var documentPath = Path.Combine(sampleProjectFolder, RelativeDocumentPaths.Get(codeGenerator.ProjectType), codeGenerator.GetDocumentName(1) + ".cs");
-                File.WriteAllText(documentPath, codeGenerator.GenerateClasses(1000, 1)[0]);
-            }
-
-            Console.WriteLine($"Project files generated to {sampleProjectFolder}");
-            Console.WriteLine("Press any key to continue...");
-            Console.ReadKey();
         }
     }
 }
